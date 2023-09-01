9d1ba482ebfc8a133479d7ab52c26d460dc4ed77
anjalipundir <anjalipundir@gmail.com>
2023-08-31 22:54:01 -0400

    Initial Check-in

---
+++ src/main/java/org/example/controller/QuickSampleController.java
@@ -0,0 +1,51 @@
+package org.example.controller;
+
+import org.example.model.QuickData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/very_quick")
+public class QuickSampleController {
+
+    /**
+     * Low Level Design - API endpoints - of this product based on below scenario:
+     * Insert data
+     * Update data
+     * Fetch all data
+     * Delete data
+     *
+     * Design simple 4 services:
+     * /very_quick
+     *  POST (create data - Non-Idempotent )
+     *  PUT (update data)
+     *  GET
+     *  DELETE
+     */
+
+    @PostMapping
+    public ResponseEntity<QuickData> createData(QuickData data){
+        // Real scenario: service request for creation
+        return new ResponseEntity<>(data, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<QuickData> updateData(QuickData data){
+        // Real scenario: service request to update
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<QuickData> getData(){
+        // Real scenario: service request to getData
+        return new ResponseEntity<>(new QuickData("sample","quick"), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteData(String name){
+        // Real scenario: service request for deletion by name
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/org/example/controller/QuickSampleControllerTest.java
@@ -0,0 +1,50 @@
+package org.example.controller;
+
+import org.example.model.QuickData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class QuickSampleControllerTest {
+
+    @Autowired
+    private QuickSampleController controller;
+
+    private QuickData data;
+
+    @BeforeEach
+    void setUp() {
+        data = new QuickData("sample","quick");
+    }
+
+    @Test
+    void createData() {
+        ResponseEntity<QuickData> response = controller.createData(data);
+        assertEquals(response.getBody(), data);
+    }
+
+    @Test
+    void updateData() {
+        ResponseEntity<QuickData> response = controller.updateData(data);
+        assertEquals(response.getBody(), data);
+    }
+
+    @Test
+    void getData() {
+        ResponseEntity<QuickData> response = controller.getData();
+        assertEquals(response.getBody(), data);
+    }
+
+    @Test
+    void deleteData() {
+        ResponseEntity<String> response = controller.deleteData(data.getName());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+}
